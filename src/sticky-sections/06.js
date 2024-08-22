b2df56065aa58e03f96de11c5bb5c24835ebacf4
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-08-22 09:50:08 -0700

    fix for 06

--- src/sticky-sections/06.js
+++ src/sticky-sections/06.js
@@ -1,33 +1,34 @@
-import { gsap } from 'https://cdn.skypack.dev/gsap';
-import { ScrollTrigger } from 'https://cdn.skypack.dev/gsap/ScrollTrigger';
+import { gsap } from "https://cdn.skypack.dev/gsap";
+import { ScrollTrigger } from "https://cdn.skypack.dev/gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const scroll = () => {
-  const panels = Array.from(document.querySelectorAll('.sticky-section_panel'));
+  const panels = Array.from(document.querySelectorAll(".sticky-section_panel"));
 
   panels.forEach((panel, index) => {
     const isLast = index === panels.length - 1;
 
-    gsap.set(panel, { transformOrigin: isLast ? 'center center' : 'top center' });
+    gsap.set(panel, {
+      transformOrigin: isLast ? "center center" : "top center",
+    });
 
     gsap
       .timeline({
         scrollTrigger: {
           trigger: panel,
-          start: 'top top',
-          end: isLast ? '+=50%' : 'bottom top',
+          start: "top top",
+          end: isLast ? "+=50%" : "bottom top",
           scrub: true,
-          markers: true,
         },
       })
       .to(panel, {
-        ease: 'none',
+        ease: "none",
         scale: 0,
       });
   });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   scroll();
 });
